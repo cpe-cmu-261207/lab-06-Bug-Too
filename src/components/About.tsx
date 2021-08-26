0e3d6644386d2d8505aba4dcfbe1a0c769568e2d
Purich <purichseenuallae@gmail.com>
2021-08-26 16:45:32 +0700

    Add Component

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        // template for about me 
+        < div className = 'text-center space-y-3' >
+                  <p className='text-2xl font-semibold'>About me</p>
+                  <p className='text-xl'>Purich Seenuallae 630610752</p>
+        </div >
+    )
+}
+
+export default About
